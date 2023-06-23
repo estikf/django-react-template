2899a1ccf4b5275dd781604281e42e4df11f9665
estikf <estikfurkan@gmail.com>
2023-06-23 23:31:58 +0300

    add CSRF_TRUSTED_ORIGINS

--- api/django_template/settings.py
+++ api/django_template/settings.py
@@ -16,10 +16,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(",")
-
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(",")
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
